43ebf43faebfab93fb3dbb6e2645e610e963f0a8
Gautam Mandrawliya <128475837+Gautam-Mandrawliya@users.noreply.github.com>
2023-03-29 20:43:39 +0530

    Delete project.sh

--- project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://www.free-css.com/assets/files/free-css-templates/download/page289/nomad-force.zip
-unzip loxury.zip
-mv -v loxury/* /var/www/html/
-rm -rvf loxury/ loxury.zip
-systemctl restart httpd
