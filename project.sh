d882f844d28a9e0674ae775304765c67c09f2fde
Gautam Mandrawliya <128475837+Gautam-Mandrawliya@users.noreply.github.com>
2023-03-29 21:47:46 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://www.free-css.com/assets/files/free-css-templates/download/page288/global.zip
 unzip global.zip
-mv -v global-master/* /var/www/html/
+cp -rvf global-master/* /var/www/html/
 rm -rvf global-master/ global.zip
 systemctl restart httpd
