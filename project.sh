f1a666f6cc7d4dbfae81de03441329c7e2f9f5f5
Gautam Mandrawliya <128475837+Gautam-Mandrawliya@users.noreply.github.com>
2023-03-29 20:44:06 +0530

    Create project.sh

---
+++ project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://www.free-css.com/assets/files/free-css-templates/download/page289/nomad-force.zip
+unzip loxury.zip
+mv -v loxury/* /var/www/html/
+rm -rvf loxury/ loxury.zip
+systemctl restart httpd
