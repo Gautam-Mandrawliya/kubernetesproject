c2ead82a813c14c032ebb5ead54e70dea4063637
Gautam Mandrawliya <128475837+Gautam-Mandrawliya@users.noreply.github.com>
2023-03-29 20:31:20 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum install -y httpd
-systemctl enable --now httpd
 wget https://www.free-css.com/assets/files/free-css-templates/download/page289/nomad-force.zip
 unzip loxury.zip
 mv -v loxury/* /var/www/html/
